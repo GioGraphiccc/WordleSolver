31f1d46af76f14fc3bc152aa8ca701675e7b4256
Gio <giovherr@gmail.com>
2022-05-03 18:25:24 -0400

    Updated directory

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # WordleSolver
- Here is my version of a code that can solve worlde puzzles. Its kinda basic but ye.
+Here is my version of a code that can solve worlde puzzles. Its kinda basic but ye.
+It can most likely find the answer to the worlde in 4 - 5 tries.
+
+If you want to add more words go to WordList.txt and add it from the bottom there.
+NOTE: Make sure you type enter for every word including the last one or the program wont work.
+(Every word requires a newline char at the end and im too lazy to fix that)
+
+HOW TO USE:
+1. When the progrm starts it prompts you to type in any five letter word.
+2. Input the starter word in wordle as well
+3. Refer back to the program to submit any green letters, gray letters, or yellow letter seen in the wordle app.
+4. After completing step 3 it will grant you a random word from a list that could be used in the app. Input that word in the wordle app
+5. Repeat step 3 and 4 until an answer has been found.
---
+++ WordleSolverv1.0/worldleSolver.py
@@ -0,0 +1,135 @@
+from random import randrange
+import colorama
+from colorama import Fore
+
+
+def CreateList():
+    wordList = []
+    x = 0 #used for while loop
+    word = ""
+    with open("wordList.txt", "r") as fileObject: #open file
+        for line in fileObject:     #loop through file line by line. one line contains the word plus a useless newline char at the end 
+            wordList.append(removeNewline(line)) #insert word into wordList
+    return wordList
+
+def removeNewline(line):
+    word = ""
+    x = 0
+    while(line[x] and line[x] != '\n'):
+        word = word + line[x]
+        x = x + 1
+    return word
+
+#returns updated wordList with words that dont contain a certain letter
+def removeWords(letter, wordList):
+    size = len(wordList)
+    newwordList = []
+    for x in range(size):
+        word  = wordList[x]
+        if(contains(word,letter)):
+            continue
+        else:
+            newwordList.append(word)
+    return newwordList
+
+def contains(word, letter):
+    for x in range(len(word)):
+        if(letter in word[x]):
+            return 1
+    return 0
+def containsPosition(word, letter, pos):
+    if(word[pos] == letter):
+        return 1
+    else:
+        return 0
+
+#returns a list of words that contains a certain letter in a certain position
+def NarrowGreen(letters, wordList): #returns a list with the words that contain a certain letter in a certain position
+    newList = []
+    for i in range(len(letters)):
+        sizeOfList = len(wordList)
+        if(i % 2 == 0):
+            newList.clear()
+            letter = letters[i]
+            pos = int(letters[i+1])
+            pos = pos - 1
+            for x in range(sizeOfList):
+                if(containsPosition(wordList[x], letter, pos) == 1):
+                    newList.append(wordList[x])
+            wordList = newList.copy()
+    return newList
+#returns a list of all the words that dont contain certain letters
+def NarrowGray(letters, wordList):
+    tempList = []
+    for i in range(len(letters)):
+        tempList.clear()
+        for x in range(len(wordList)):
+            if(contains(wordList[x], letters[i]) == 0):
+                tempList.append(wordList[x])
+        wordList = tempList.copy()
+    return wordList
+
+#returns a list that contains words with that letter except the letter in the certain pos
+#Used for the main func when receiving yellow letters from the user.
+def NarrowYellow(letters, wordList):
+    tempList = []
+    
+    for i in range(len(letters)):
+        if(i % 2 == 0):
+            tempList.clear()
+            letter = letters[i]
+            pos = int(letters[i+1])
+            pos = pos - 1
+            
+            for x in range(len(wordList)):
+                if(containsPosition(wordList[x], letter, pos) == 0): #create new list of words that dont have that letter in that pos
+                    tempList.append(wordList[x])
+            wordList = tempList.copy()
+            tempList.clear()
+            for x in range(len(wordList)):
+                if(contains(wordList[x], letter) == 1): #create a new list that contains the letter 
+                    tempList.append(wordList[x])
+            
+    wordList = tempList.copy()
+    return wordList
+                    
+
+wordList = CreateList()
+proceed = 1
+tempList = []
+print("Hello and welcome to Worlde Solver v1.0!")
+print("This solver will most likely solve the word in 4-5 tries.")
+print("---------------------------------------\n")
+starterWord = input("What's your starter word gonna be today boss?\n-")
+wordList.remove(starterWord)
+while(proceed == 1):
+    print("If there are any", end=" ")
+    print(Fore.GREEN + "GREEN", end=" ")
+    print(Fore.WHITE + "letters type in the letter followed by the positon no spaces (Ex. a2b4c1)\n")
+    greenletters = input("If there are none type none\n-")
+    if(greenletters != "none"):
+        templist = NarrowGreen(greenletters, wordList).copy()
+        wordList = templist.copy()
+        tempList.clear()
+    else:
+        print("sucks")
+    grayLetters = input("If there are any GRAY letters type them in all together(Ex. abc). \nIf there are none type none\n-")
+    if(grayLetters != "none"):
+        tempList = NarrowGray( grayLetters, wordList).copy()
+        wordList = tempList.copy()
+        tempList.clear()
+
+    print("If there are any", end=" ")
+    print(Fore.YELLOW + "YELLOW",end=" ")
+    yellowLetters = input(Fore.WHITE + "letters, type the letter followed by the position no spaces(Ex. a2b4c1)\nIf there are none type none\n-")
+    if(yellowLetters != "none"):
+        tempList = NarrowYellow(yellowLetters, wordList).copy()
+        wordList = tempList.copy()
+        tempList.clear()
+    randomNum = randrange(len(wordList))
+    randomWord = wordList[randomNum]
+    print("My calculations indicate to use the word \"" + randomWord + "\"")
+    wordList.remove(randomWord)
+
+
+
